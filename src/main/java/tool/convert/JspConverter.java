a8f2a009280320fbe99a0073b4b9bcee6cc4a30f
namickey <teethdai@gmail.com>
2018-04-02 22:44:13 +0900

    add init files.

---
+++ src/main/java/tool/convert/JspConverter.java
@@ -0,0 +1,18 @@
+package tool.convert;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JspConverter {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}

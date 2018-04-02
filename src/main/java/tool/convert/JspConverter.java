6719d205187a1a7842fcb4f2bb4c8d1548ba6e26
namickey <teethdai@gmail.com>
2018-04-02 23:19:37 +0900

    impl converter.

--- src/main/java/tool/convert/JspConverter.java
+++ src/main/java/tool/convert/JspConverter.java
@@ -1,18 +1,26 @@
 package tool.convert;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.*;
 
 public class JspConverter {
 
-    public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) throws IOException {
+        File dir = new File("work/in/");
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            convertFile(file);
+        }
     }
 
+    public static void convertFile(File inFile) throws IOException {
+        File outFile = new File("work/out/"+inFile.getName());
+        try(BufferedReader b_reader = new BufferedReader(new InputStreamReader(new FileInputStream(inFile),"Shift_jis"));
+        PrintWriter p_writer    = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile),"UTF-8")))){
+            String s;
+            while((s = b_reader.readLine())!=null){
+                p_writer.println(s);
+            }
+        }
+    }
 }
